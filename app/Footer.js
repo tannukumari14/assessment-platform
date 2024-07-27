750b9131d6d08217fc9ee76fb0f3332121eca98d
navgurukul <ranisingh21@navgurukul.org>
2024-07-27 17:30:00 +0530

    header mobile view done

--- app/Footer.js
+++ app/Footer.js
@@ -3,7 +3,7 @@ const  Footer = () => {
         <div className="footer">
             <div className="footer-p">
                 <p className="p-footer">Footer</p>
-                <p>© Copyright, All Rights Reserved by Ungineering</p>
+                <p className="p">© Copyright, All Rights Reserved by Ungineering</p>
             </div>
         </div>
     )
