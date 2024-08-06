3dd9db802f4fe8a3ca6c6d748946e9aee0f7e202
navgurukul <ranisingh21@navgurukul.org>
2024-08-06 19:17:45 +0530

    3rd page dekstop

--- app/component/Assessment_Question_Page.js
+++ app/component/Assessment_Question_Page.js
@@ -12,10 +12,10 @@ const Assessment_Question_Page = () => {
                             <div className="numbers">15</div>
                             <div className="numbers">20</div>
                         </div>
-                        <p className="assessment-p">Question Attempted</p>
+                        <p className="assessment-p">Questions Attempted</p>
                         <div className="div">
                             <div className="all40">All(40)</div>
-                            <div className="un">Attempted</div>
+                            <div className="un">Attempted(3)</div>
                             <div className="attemp">Unattempted(37)</div>
                         </div>
                     </div>
@@ -25,7 +25,7 @@ const Assessment_Question_Page = () => {
                             <p className="question1">QUESTION 1</p>
                             <p className="score">Score +1</p>
                         </div>
-                        <div className="contaniers-questions">
+                        <div className="container-questions">
                             <div className="question1">Which of the following committees recommended inclusion of fundamental duties?</div>
                             <p className="option">Tarapore committee</p>
                             <p className="options">Radha Krishnan committee</p>
@@ -49,7 +49,7 @@ const Assessment_Question_Page = () => {
                             <p className="question1">QUESTION 3</p>
                             <p className="score">Score +1</p>
                         </div>
-                        <div className="contaniers-questions">
+                        <div className="container-questions">
                             <div className="question1">Which of the following committees recommended inclusion of fundamental duties?</div>
                             <p className="option">Tarapore committee</p>
                             <p className="options">Radha Krishnan committee</p>
@@ -61,7 +61,7 @@ const Assessment_Question_Page = () => {
                             <p className="question1">QUESTION 4</p>
                             <p className="score">Score +1</p>
                         </div>
-                        <div className="contaniers-questions">
+                        <div className="container-questions">
                             <div className="question1">Which of the following committees recommended inclusion of fundamental duties?</div>
                             <p className="option">Tarapore committee</p>
                             <p className="options">Radha Krishnan committee</p>
@@ -73,7 +73,7 @@ const Assessment_Question_Page = () => {
                             <p className="question1">QUESTION 5</p>
                             <p className="score">Score +1</p>
                         </div>
-                        <div className="contaniers-questions">
+                        <div className="container-questions">
                             <div className="question1">Which of the following committees recommended inclusion of fundamental duties?</div>
                             <p className="option">Tarapore committee</p>
                             <p className="options">Radha Krishnan committee</p>
--- app/score/Submit.js
+++ app/score/Submit.js
@@ -1,20 +1,22 @@
 const Submit = () => {
   return (
       <>
-          <div className="submit-box">
-              <img src="Ellipse1.svg" alt="Ellipse" />
-              <img src="group.svg" alt="Group" />
-              <div className="content">
-                  <p className="successfuly">Successfully Submitted</p>
-                  <p className="thankyou">Thank you for completing the test!</p>
-                  <p className="review">
-                      Review your results and keep practicing. 
-                      Great job and keep up the hard work!
-                  </p>
-              </div>
-              <button className="submit-btn">Check Your Score</button>
-          </div>
-      </>
+        <div className="submit-box">
+            <img src="Ellipse1.svg" alt="Ellipse" />
+            <img src="group.svg" alt="Group" />
+             <div className="content">
+                <p className="successfuly">Successfully Submitted</p>
+                <p className="thankyou">Thank you for completing the test!</p>
+                <p className="review">
+                    Review your results and keep practicing. 
+                    Great job and keep up the hard work!
+                </p>
+            </div>
+            <div className="btn">
+                <button className="submit-btn">Check Your Score</button>
+            </div>
+        </div>
+      </>   
   );
 }
 
