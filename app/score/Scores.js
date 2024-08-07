bc20ebb932d9c87b534be21cb4d6b2895fdd99da
navgurukul <ranisingh21@navgurukul.org>
2024-08-07 16:49:42 +0530

    4th page score

---
+++ app/score/Scores.js
@@ -0,0 +1,27 @@
+const Scores=()=>{
+    return(
+        <>
+        <div className="base">
+            <div className="h1-score">
+                <h1>35/40</h1>
+                <p>Your Scored</p>
+            </div>
+            <div className="Unattempted">
+                <img src="exmilatio.svg"></img>
+                <p>Unattempted: 3</p>
+            </div>
+            <hr></hr>
+            <div className="image-text">
+                <img src="Check.png"></img>
+                <p>Correct: 35</p>
+            </div>
+            <hr></hr>
+            <div className="image-text">
+                <img src="wrong.svg"></img>
+                <p>Wrong: 2</p>
+            </div>
+        </div>
+        </>
+    )
+}
+export default Scores;
--- app/score/page.js
+++ app/score/page.js
@@ -1,10 +1,12 @@
 import Score_Header from "./Score_Header"
+import Scores from "./Scores";
 import "../globals.css";
 
 const page=()=>{
     return(
         <>
         <Score_Header/>
+        <Scores/>
         </>
     )
 }
