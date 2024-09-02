c6fe975bce42b48d0f451c3cc2884c5d44c7d286
navgurukul <ranisingh21@navgurukul.org>
2024-09-02 19:38:42 +0530

    desktop and mobie view header and pagination

--- app/component/Assessment_Header.js
+++ app/component/Assessment_Header.js
@@ -6,17 +6,21 @@ const Assessment_Header = () => {
     return (
         <>
             <div className='bgcolorwhitefullwidth'>
-                <div className="assessment-header">
-                    <div className="images-logo">
-                        <div className='two-logo'>
-                            <img className="logo1" src="LogoGrp.svg" alt="Logo Group" />
-                            <img className="logo2" src="QA.png" alt="QA Logo" />
-                        </div>
-                        <div className='pagination-Div'>
-                            <Pagination />
+                <div style={{ justifyContent: 'space-between' }} className="assessment-header">
+                    <div style={{ width: '998px'}}>
+                        <div className="images-logo">
+                            <div className='two-logo'>
+                                <img className="logo1" src="LogoGrp.svg" alt="Logo Group" />
+                                <img className="logo2" src="QA.png" alt="QA Logo" />
+                            </div>
+                            <div className='pagination-Div'>
+                                <Pagination />
+                            </div>
                         </div>
                     </div>
-                    <Link href="submit" className="submit-assessment">Submit</Link>
+                    <div>
+                        <Link href="submit" className="submit-assessment">Submit</Link>
+                    </div>
                 </div>
             </div>
         </>
