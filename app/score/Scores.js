fc00567e1b31f949e99a8a44a10e2e2563d4ed16
navgurukul <ranisingh21@navgurukul.org>
2024-09-03 16:53:49 +0530

    page 4

--- app/score/Scores.js
+++ app/score/Scores.js
@@ -4,24 +4,26 @@ const Scores = () => {
     return (
         <>
             <div className="base">
-                <div className="h1-score">
-                    <h1>35/40</h1>
-                    <p>Your Scored</p>
-                </div>
-                <div className="div-for-score">
-                    <div className="Unattempted">
-                        <img src="exmilatio.svg"></img>
-                        <p>Unattempted:  3</p>
+                <div className="base1">
+                    <div className="h1-score">
+                        <h1 className="thirty-fourth">35/40</h1>
+                        <p className="your-score-Score">Your Scored</p>
                     </div>
-                    <hr className="hrs"></hr>
-                    <div className="image-text">
-                        <img src="Check.png"></img>
-                        <p>Correct:  35</p>
-                    </div>
-                    <hr className="hrs"></hr>
-                    <div className="image-text">
-                        <img src="wrong.svg"></img>
-                        <p>Wrong:  2</p>
+                    <div className="div-for-score">
+                        <div className="Unattempted">
+                            <img src="exmilatio.svg"></img>
+                            <p>Unattempted:  3</p>
+                        </div>
+                        <hr className="hrs"></hr>
+                        <div className="image-text">
+                            <img src="Check.png"></img>
+                            <p>Correct:  35</p>
+                        </div>
+                        <hr className="hrs"></hr>
+                        <div className="image-text">
+                            <img src="wrong.svg"></img>
+                            <p>Wrong:  2</p>
+                        </div>
                     </div>
                 </div>
             </div>
