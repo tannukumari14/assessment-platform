6ba84c72cfa4ed0088519b576a237a5b8bd92ab2
navgurukul <ranisingh21@navgurukul.org>
2024-09-03 14:59:38 +0530

    page 3 box

--- app/component/Assessment_Header.js
+++ app/component/Assessment_Header.js
@@ -1,7 +1,6 @@
 import Pagination from './Pagination';
 import './Assessment_Header.css';
 import Link from 'next/link';
-import "./Pagination.css";
 
 const Assessment_Header = () => {
     return (
@@ -15,22 +14,7 @@ const Assessment_Header = () => {
                                 <img className="logo2" src="QA.png" alt="QA Logo" />
                             </div>
                             <div className='pagination-Div'>
-                                {/* <Pagination /> */}
-
-        <div className="box">
-            <img className="previous" src="Component1.svg" alt="Previous" />
-            <a className="circle1">1</a>
-            <p>...</p>
-            <a className="circle" >2</a>
-            <a className="circle">3</a>
-            <a className="circle">4</a>
-            <a className="circle">5</a>
-            <p>...</p>
-            <a className="circle">6</a>
-            <a className="circle">7</a>
-            <a className="circle">8</a>
-            <img className="next" src="Component2.svg" alt="Next" />
-        </div>
+                                <Pagination />
                             </div>
                         </div>
                     </div>
