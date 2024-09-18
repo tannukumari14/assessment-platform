4d87e7c19e9a580d0b086c416eb563e29c0ba246
Tanu <tannukumari21@navgurukul.org>
2024-09-18 15:42:30 +0530

    rewrite code for clock

--- app/component/Clock.js
+++ app/component/Clock.js
@@ -6,8 +6,10 @@ const Clock = () => {
             <div className="clock-container">
                 <div className="cercle1">
                     <div className="cercle2">
-                        <div className="second">50 : 20</div>
-                        <p className="min-sec">min sec</p>
+                        <div className="sec-min">
+                            <div className="second">50 : 20</div>
+                            <p className="min-sec">min sec</p>
+                        </div>
                     </div>
                 </div>
                 <div className="time">Remaining Time</div>
