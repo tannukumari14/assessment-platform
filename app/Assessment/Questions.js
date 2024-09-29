5724c16e314381221f512bec40d2f324e662dd4c
tannukumari14 <tannukumari21@navgurukul.org>
2024-09-29 22:10:58 +0530

    data fetch from API

--- app/Assessment/Questions.js
+++ app/Assessment/Questions.js
@@ -1,10 +1,25 @@
+"use client";
+import { useEffect, useState } from "react";
+
 import "./Assessment_Question_Page.css";
 
 const Questions=()=>{
+
+    const [data,setData] = useState([]);
+
+    useEffect(() => {
+        fetch(`https://the-trivia-api.com/v2/questions`)
+            .then(response => response.json())
+            .then((data) => {
+                console.log(data);
+                setData(data);
+            });
+    }, []);
+
     return(
         <>
         <div className="question-border">
-            <div className="p-tag">
+            {/* <div className="p-tag">
                 <p className="question1">QUESTION 1</p>
                 <p className="score">Score +10</p>
             </div>
@@ -14,7 +29,16 @@ const Questions=()=>{
                 <p className="options">Radha Krishnan Committee</p>
                 <p className="options">Swaran Singh Committee</p>
                 <p className="options1">Balwantrai Mehta Committee</p>
-            </div>
+            </div> */}
+                {data.map((item) => (
+                <div key={item.id}>
+                    <h2>question:-{item.question.text}</h2> 
+                    <br></br>
+                    <h3>correctAnswer:-{item.correctAnswer}</h3>
+                    <br></br>
+                    <h3>incorrectAnswers:-{item.incorrectAnswers}</h3>
+                </div>
+            ))}
         </div>
         </>
     )
